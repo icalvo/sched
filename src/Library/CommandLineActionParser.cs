d7bb49fbd5f25a6ab915f78d9ed5beeae6d8e0be
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2022-05-05 21:12:56 +0200

    Implemented CommandLineActionParser

--- src/Library/CommandLineActionParser.cs
+++ src/Library/CommandLineActionParser.cs
@@ -1,9 +1,34 @@
+using System.Diagnostics;
+
 namespace Scheduler;
 
 public class CommandLineActionParser : IActionParser
 {
-    public Func<CancellationToken, Task> ParseAction(string actionId)
+    public Func<CancellationToken, Task> ParseAction(string commandLine)
     {
-        return _ => Task.CompletedTask;
+        return _ =>
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {commandLine}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+ 
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+            
+            return Task.CompletedTask;
+        };
     }
 }
--- src/Library/MockActionParser.cs
+++ src/Library/MockActionParser.cs
@@ -2,10 +2,10 @@ namespace Scheduler;
 
 public class MockActionParser : IActionParser
 {
-    public Func<CancellationToken, Task> ParseAction(string actionId) =>
+    public Func<CancellationToken, Task> ParseAction(string commandLine) =>
         _ =>
         {
-            Console.WriteLine($"Done action {actionId}");
+            Console.WriteLine($"Done action {commandLine}");
             return Task.CompletedTask;
         };
 }
