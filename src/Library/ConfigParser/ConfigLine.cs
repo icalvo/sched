85f2c69a4f17530d51481c003bf524686efa9d97
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2022-05-07 19:41:55 +0200

    Implemented new date interval line format

--- src/Library/ConfigParser/ConfigLine.cs
+++ src/Library/ConfigParser/ConfigLine.cs
@@ -10,46 +10,50 @@ public abstract class ConfigLine
 {
     public static ConfigLine Parse(string line)
     {
-        static Option<DateOnly> ParseDate(string date)
+        static Option<DateOnly> ParseEndpointDate(string date)
             => date == "..." ? None : Some(DateOnly.Parse(date));
+        string[] split;
+
+        if (line.StartsWith("R "))
+        {
+            return new RandomizeConfig(TimeSpan.Parse(line[2..]));
+        }
 
         if (line.StartsWith("+"))
         {
-            var split = line[1..].Split(" ", 2, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            split = line[1..].Split(" ", 2, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
             return new AliasConfig(split[0], split[1]);
         }
-        
-        if (line.StartsWith("-"))
+
+        split = line.Split(" ", 6, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        if (line.StartsWith("-") || split[0] == "..." || DateOnly.TryParse(split[0], out _))
         {
-            var split = line[1..].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            if (line.StartsWith("-"))
+            {
+                line = line[1..];
+            }
+
+            split = line.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
             return split.Length switch
             {
                 0 => new IntervalHeader(Interval.All<DateOnly>()),
                 1 =>
                     new IntervalHeader(
                         Interval.Create(
-                            ParseDate(split[0]),
-                            ParseDate(split[0]).Map(x => x.AddDays(1)))),
+                            ParseEndpointDate(split[0]),
+                            ParseEndpointDate(split[0]).Map(x => x.AddDays(1)))),
                 2 =>
                     new IntervalHeader(
                         Interval.Create(
-                            ParseDate(split[0]),
-                            ParseDate(split[1]).Map(x => x.AddDays(1)))),
+                            ParseEndpointDate(split[0]),
+                            ParseEndpointDate(split[1]).Map(x => x.AddDays(1)))),
                 _ => throw new ArgumentException($"Too many parts in \"{line}\"", nameof(line))
             };
         }
 
-        if (line.StartsWith("R "))
-        {
-            return new RandomizeConfig(TimeSpan.Parse(line[2..]));
-        }
-        else
-        {
-            var split = line.Split(" ", 6, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-            var cronSplit = split[..5].Reverse().StringJoin(" ");
-            var cronExpression = CronExpression.Parse(cronSplit);
-            return new TaskConfig(split[5], cronExpression);
-        }
+        var cronSplit = split[..5].Reverse().StringJoin(" ");
+        var cronExpression = CronExpression.Parse(cronSplit);
+        return new TaskConfig(split[5], cronExpression);
     }    
 }
