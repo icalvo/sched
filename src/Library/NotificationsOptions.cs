be6d5086c2789fa0908286f2cc86e5abd272f14e
Ignacio Calvo <ignacio.calvo@gmail.com>
2022-06-16 08:05:14 +0200

    Optional Notification Options

--- src/Library/NotificationsOptions.cs
+++ src/Library/NotificationsOptions.cs
@@ -2,6 +2,24 @@ namespace Scheduler;
 
 public class NotificationsOptions : INotificationsOptions
 {
+    public NotificationsOptions() : this(null)
+    {
+    }
+
+    public NotificationsOptions(
+        string[]? nextEvent = null,
+        string[]? configurationChanged = null,
+        string[]? configurationChangeError = null,
+        string[]? commandSucceeded = null,
+        string[]? commandFailed = null)
+    {
+        NextEvent = nextEvent ?? Array.Empty<string>();
+        ConfigurationChanged = configurationChanged ?? Array.Empty<string>();
+        ConfigurationChangeError = configurationChangeError ?? Array.Empty<string>();
+        CommandSucceeded = commandSucceeded ?? Array.Empty<string>();
+        CommandFailed = commandFailed ?? Array.Empty<string>();
+    }
+
     public string[] NextEvent { get; set; }
     public string[] ConfigurationChanged { get; set; }
     public string[] ConfigurationChangeError { get; set; }
